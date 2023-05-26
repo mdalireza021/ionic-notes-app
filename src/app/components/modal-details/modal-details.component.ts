def839aefb23a20e4bf73e1f6e8924e20f7e1288
BS1096 <bs1096@BS1096s-Laptop.local>
2023-05-26 19:54:35 +0600

    update modal

--- src/app/components/modal-details/modal-details.component.ts
+++ src/app/components/modal-details/modal-details.component.ts
@@ -1,14 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal-details',
   templateUrl: './modal-details.component.html',
   styleUrls: ['./modal-details.component.scss'],
+  standalone: true,
+  imports: [IonicModule,CommonModule,FormsModule]
 })
 export class ModalDetailsComponent  implements OnInit {
 
-  constructor() { }
+  noteTitle!: string;
+  noteDesc!: string;
+
+  description!: string;
+  textareaHeight = '100%';
+
+  constructor(private modalCtrl: ModalController) {}
 
   ngOnInit() {}
+
+  cancel() {
+    return this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    return this.modalCtrl.dismiss('confirm');
+  }
 
 }
